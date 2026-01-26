72040eef0c69c5f3ec4c46d485cca46f723abc5e
Logan Lindquist Land <logan@llbbl.com>
2026-01-25 19:09:18 -0600

    Fix port mismatch in Koa fixture
    ### Fixes
    - Changed server to listen on port 3000 to match Dockerfile EXPOSE directive
    - Updated console log to reflect correct port

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { createApp } from './app.js'
 
 const app = createApp()
 
-app.listen(8080, () => {
-  console.log('Server running on http://localhost:8080')
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000')
 })
